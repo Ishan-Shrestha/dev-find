5beaf74610c090ff87b3a342c6246b9e08f667da
Ishan-Shrestha <sthaishan218@gmail.com>
2026-06-09 23:16:24 +0545

    add 10 minute response caching.

--- fetch.py
+++ fetch.py
@@ -1,5 +1,6 @@
 import logging
 import requests
+import time
 
 # LOGGER CONFIG
 logging.basicConfig(
@@ -11,18 +12,28 @@ logger.setLevel(logging.DEBUG)
 
 # VARIABLES
 API= 'https://remoteok.com/api'
+data = []
+last_update_time = 0
 
 # LOGICAL FUNCTIONS
 def get_data():
     logger.info("Attempting to retrieve data!")
-    try:
-        response = requests.get(API, timeout=5)
-        logger.info("Data retrieved successfully!")
-        return response.json()
-    except Exception as e:
-        logger.exception("Failed to retrieve data")
-        return f"Unexpected error occured: {e}"
-    
+    global data, last_update_time
+    current_time = time.time()
+    if not data or (current_time-last_update_time>=600):
+        try:
+            response = requests.get(API, timeout=5)
+            logger.info("Data retrieved successfully!")
+            data = response.json()
+            last_update_time= current_time
+            return data
+        except Exception as e:
+            logger.exception("Failed to retrieve data")
+            return f"Unexpected error occured: {e}"
+    else:
+        return data
+
+
 # TEST 
 # data = get_data()
 # n = int(input('Specify the number of jobs to list: '))
